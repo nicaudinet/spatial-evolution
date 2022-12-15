1a2a3752f42b013c9dd0e8980082bd8c221dd589
Nicolas Audinet <nic.audinet27@gmail.com>
2022-12-15 15:29:24 +0100

    Split constants out into lib.constants

--- game.py
+++ game.py
@@ -8,23 +8,8 @@ import itertools
 import matplotlib.pyplot as plt
 
 from lib.player import Player
-
-C = 'c' # Cooperate
-D = 'd' # Defect
-A = 'a' # Abstain
-
-ALL_ACTIONS = [C,D,A]
-
-# T > R > L > P > S
-# to compare with prisoners dilemma
-T = 5
-R = 3
-L = 2
-P = 1
-S = 0
-
-P1 = 0
-P2 = 1
+from lib.constants import *
+from lib.history import History
 
 class History:
     def __init__(self):
---
+++ lib/constants.py
@@ -0,0 +1,16 @@
+C = 'c' # Cooperate
+D = 'd' # Defect
+A = 'a' # Abstain
+
+ALL_ACTIONS = [C,D,A]
+
+# T > R > L > P > S
+# to compare with prisoners dilemma
+T = 5
+R = 3
+L = 2
+P = 1
+S = 0
+
+P1 = 0
+P2 = 1
